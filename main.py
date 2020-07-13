3b29f2270730b6b980e5d895db890a4caeca6ef7
Ubuntu <ubuntu@ip-172-31-26-165.ap-northeast-1.compute.internal>
2020-07-13 02:24:52 +0000

    Update to set appropriate clock

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import hydra
 from logging import getLogger
 import subprocess
+import pandas
 
 
 logger = getLogger(__name__)
 
 
+def get_clock(csv_path, l1size, l1assoc):
+    df = pandas.read_csv(csv_path)
+    access_time = df[(df['cache_size'] == l1size) & (df['associativity'] == l1assoc)]['access_time[ns]']
+    clock = float(1e9 / access_time)
+    return clock
+
+
 @hydra.main(config_path='config.yaml')
 def main(config):
+    clock = get_clock(hydra.utils.to_absolute_path(config.access_time.csv_path), config.l1size, config.l1assoc)
     command = []
-    for key, value in config.items():
+    for key, value in config.command.items():
         logger.debug(key)
         logger.debug(value)
-        if isinstance(value, bool) and value:
+        if value is None:
+            if key == '--cpu-clock':
+                command += [f"{key}={clock}Hz"]
+        elif isinstance(value, bool) and value:
             command += [str(key)]
         else:
             command += [f"{key}={value}"]
+        logger.debug(command[-1])
     logger.info(command)
     result = subprocess.run(command)
     logger.info(result)
-
 
 
 if __name__ == "__main__":
