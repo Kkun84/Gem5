0fdbdb9b1d9e5846418456be39fbd1be147614c7
Ubuntu <ubuntu@ip-172-31-26-165.ap-northeast-1.compute.internal>
2020-07-20 01:27:21 +0000

    Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hydra
+from hydra.utils import to_absolute_path
 from logging import getLogger
 import subprocess
 import pandas
@@ -10,6 +11,7 @@ logger = getLogger(__name__)
 def get_clock(csv_path, l1size, l1assoc):
     df = pandas.read_csv(csv_path)
     access_time = df[(df['cache_size'] == l1size) & (df['associativity'] == l1assoc)]['access_time[ns]']
+    print(access_time)
     clock = float(1e9 / access_time)
     return clock
 
@@ -26,9 +28,10 @@ def get_from_stats(text_dir, param):
     return s_list
 
 
-def write_sim_ticks(csv_path, core, l1size, l1assoc, l2size, l2assoc, latency, save_params):
+def write_sim_ticks(csv_path, core, clock, l1size, l1assoc, l2size, l2assoc, latency, save_params):
     df = pandas.DataFrame(dict(
         core=[core],
+        clock=[clock],
         l1size=[l1size],
         l1assoc=[l1assoc],
         l2size=[l2size],
@@ -36,17 +39,17 @@ def write_sim_ticks(csv_path, core, l1size, l1assoc, l2size, l2assoc, latency, s
         latency=[latency],
         **save_params
     ))
-    df.to_csv(csv_path, mode='a', header=None)
+    df.to_csv(csv_path, mode='a', header=False)
     return
 
 
 @hydra.main(config_path='config.yaml')
 def main(config):
-    clock = get_clock(hydra.utils.to_absolute_path(config.access_time.csv_path), config.l1size, config.l1assoc)
+    clock = get_clock(to_absolute_path(config.access_time.csv_path), config.l1size, config.l1assoc)
     command = []
     for key, value in config.command.items():
-        logger.debug(key)
-        logger.debug(value)
+        logger.info(key)
+        logger.info(value)
         if value is None:
             if key == '--cpu-clock':
                 command += [f"{key}={clock}Hz"]
@@ -54,10 +57,16 @@ def main(config):
             command += [str(key)]
         else:
             command += [f"{key}={value}"]
-        logger.debug(command[-1])
+        logger.info(command[-1])
     logger.info(command)
     result = subprocess.run(command)
     logger.info(result)
+
+    with open(f"{config.outdir}stats.txt") as f:
+        s = f.read()
+        if not s:
+            logger.info('File is empty.')
+            return
 
     save_params = dict()
     for param_name in config.result.save_params:
@@ -68,6 +77,11 @@ def main(config):
         else:
             save_params[param_name] = param[0]
     logger.info(save_params)
+
+    logger.info('Save CSV.')
+    write_sim_ticks(to_absolute_path(config.result.csv_path), config.core, clock, config.l1size, config.l1assoc, config.l2size, config.l2assoc, config.latency, save_params)
+
+    logger.info('Done all.')
 
 
 if __name__ == "__main__":
